45db182cfcc2d79129eb0768fbd46f9e2efeb088
sablank725 <sablank725@gmail.com>
2014-02-01 16:59:31 -0600

    Commit Test
    Trying to learn about Git Version Control - SB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResumeApp::Application.config.secret_key_base = 'c732598b796c7ac898538f54fad6b96af6898b9113066960ea5b76443b93e9039ac43ffec8a0c6daa857674b601b4412cbd1fa2838a19dde45dfbad49a20cabb'
